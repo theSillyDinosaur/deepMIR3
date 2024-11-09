76b096a08664850e3c1f66d9e13ba582bf08ff38
theSillyDinosaur <franshsie0928@gmail.com>
2024-11-09 13:46:09 +0800

    apply new batch setting

--- Huggingface/Train.py
+++ Huggingface/Train.py
@@ -14,12 +14,15 @@ def get_Model(name):
         config=GPT2Config()
         model=GPT2LMHeadModel(config)
     elif name=="TransfoXL":
-        config=TransfoXLConfig()
+        config=TransfoXLConfig(
+            n_head=12,
+            n_layer=12
+        )
         model=TransfoXLLMHeadModel(config)
     return model
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", type=str, choices=["GPT2, TransfoXL"], default='GPT2')
+parser.add_argument("--model", type=str, choices=["GPT2", "TransfoXL"], default='GPT2')
 parser.add_argument("--pop1k7", type=str, default='Pop1K7_REMI')
 parser.add_argument("--ckpt", type=str, default='result')
 parser.add_argument("--n_epoch", type=int, default=3)
@@ -34,6 +37,8 @@ training_args = TrainingArguments(
     evaluation_strategy="epoch",
     per_device_train_batch_size=args.batch_size,
     num_train_epochs=args.n_epoch,
+    warmup_steps = 16000,
+    save_steps=16000
 )
 
 # Initialize Trainer without specifying the tokenizer
